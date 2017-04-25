f2fd43014052459c71eb6e51e74c226c569893e4
FelipeHarlen <felipeharlen@hotmail.com>
2017-04-25 08:40:33 -0300

    Classe My440Remote adicionada.

---
+++ src/mypetrmi/My440Remote.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypetrmi;
+
+import java.io.Serializable;
+
+
+
+/**
+ * 
+ * This class implements an interface for a remote object
+ * 
+ *
+ */
+
+public interface My440Remote extends Serializable {
+    
+}
